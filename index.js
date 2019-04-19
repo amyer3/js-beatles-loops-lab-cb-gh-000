4d48b231fe39e5ae8f944c0b473d3e32d6c784ac
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-19 17:09:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for (var i = 0, i < musicians.length(), i++) {
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array
+}
+
+function johnLennonFacts(){}
+
+function iLoveTheBeatles(){}
